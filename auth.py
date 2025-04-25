2a0d17bf42f7cd218c85ce2e091a3f49c2c4202f
bernackimark <bernackimark@gmail.com>
2025-04-25 17:08:21 -0400

    pointed TOKEN to config; commented out auth re-try flow

--- auth.py
+++ auth.py
@@ -1,19 +1,17 @@
 import json
-import os
-from pathlib import Path
+# from pathlib import Path
 
+from config import GOOGLE_TOKEN
 from google.oauth2.credentials import Credentials
 
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly', 'https://www.googleapis.com/auth/spreadsheets.readonly']
-TOKEN_FILE = Path("token.json")
-CLIENT_SECRET_FILE = Path("client_secret.json")
+# TOKEN_FILE = Path("token.json")
+# CLIENT_SECRET_FILE = Path("client_secret.json")
 
 def get_creds() -> Credentials:
-    google_token = os.getenv("GOOGLE_TOKEN")
-
-    if not google_token:
+    if not GOOGLE_TOKEN:
         raise ValueError("Google OAuth token not found in environment variable 'GOOGLE_TOKEN'")
 
-    token_info = json.loads(google_token)
+    token_info = json.loads(GOOGLE_TOKEN)
 
     return Credentials.from_authorized_user_info(token_info, SCOPES)
